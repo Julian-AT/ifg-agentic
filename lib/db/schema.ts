1b83f791ac8ad1c927f9b9959a709b3f9689f98f
Julian-AT <julian.schmidt1005@gmail.com>
2025-07-22 14:15:18 +0200

    initial commit

--- lib/db/schema.ts
+++ lib/db/schema.ts
@@ -109,9 +109,9 @@ export const document = pgTable(
     createdAt: timestamp("createdAt").notNull(),
     title: text("title").notNull(),
     content: text("content"),
-    kind: varchar("text", { enum: ["text", "code", "image", "sheet"] })
+    kind: varchar("text", { enum: ["code"] })
       .notNull()
-      .default("text"),
+      .default("code"),
     userId: uuid("userId")
       .notNull()
       .references(() => user.id),
